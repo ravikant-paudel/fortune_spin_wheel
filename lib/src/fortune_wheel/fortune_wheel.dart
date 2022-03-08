501f0196257b28ba24e848f838ca4df40d3d33d0
Ravikant Paudel <ravikant.paudel@gmail.com>
2022-03-08 15:58:33 +0545

    Removed Unwanted Code.

--- lib/src/fortune_wheel/fortune_wheel.dart
+++ lib/src/fortune_wheel/fortune_wheel.dart
@@ -20,38 +20,6 @@ double _calculateSliceAngle(int index, int itemCount) {
   return childAngle + angleOffset;
 }
 
-// double _calculateAlignmentOffset(Alignment alignment) {
-//   if (alignment == Alignment.topRight) {
-//     return _math.pi * 0.25;
-//   }
-//
-//   if (alignment == Alignment.centerRight) {
-//     return _math.pi * 0.5;
-//   }
-//
-//   if (alignment == Alignment.bottomRight) {
-//     return _math.pi * 0.75;
-//   }
-//
-//   if (alignment == Alignment.bottomCenter) {
-//     return _math.pi;
-//   }
-//
-//   if (alignment == Alignment.bottomLeft) {
-//     return _math.pi * 1.25;
-//   }
-//
-//   if (alignment == Alignment.centerLeft) {
-//     return _math.pi * 1.5;
-//   }
-//
-//   if (alignment == Alignment.topLeft) {
-//     return _math.pi * 1.75;
-//   }
-//
-//   return 0;
-// }
-
 class _WheelData {
   final BoxConstraints constraints;
   final int itemCount;
@@ -76,92 +44,41 @@ class _WheelData {
   });
 }
 
-class FortuneWheel extends StatefulWidget {
-  @override
-  State<FortuneWheel> createState() => _FortuneWheelState();
-}
+class FortuneWheel extends StatelessWidget {
+  final List<FortuneItem> items;
 
-class _FortuneWheelState extends State<FortuneWheel> with SingleTickerProviderStateMixin {
-  late final AnimationController _animationController;
-  late final Animation<double> _rotationAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    _animationController = AnimationController(vsync: this, duration: Duration(seconds: 2));
-    _rotationAnimation = Tween<double>(begin: 0, end: 2).animate(
-      CurvedAnimation(parent: _animationController, curve: Curves.linear),
-    );
-    _animationController.forward();
-  }
+  const FortuneWheel({
+    Key? key,
+    required this.items,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final items = [
-      FortuneItem(child: Text('Apple'), style: FortuneItemStyle(color: Colors.red)),
-      FortuneItem(child: Text('Ball'), style: FortuneItemStyle(color: Colors.purple)),
-      FortuneItem(child: Text('Cat'), style: FortuneItemStyle(color: Colors.green)),
-      FortuneItem(child: Text('Doll'), style: FortuneItemStyle(color: Colors.blue)),
-      FortuneItem(child: Text('Elephant'), style: FortuneItemStyle(color: Colors.deepOrange)),
-    ];
-    return Column(
-      children: [
-        Expanded(
-          child: AnimatedBuilder(
-            animation: _rotationAnimation,
-            builder: (context, _) {
-              // final size = MediaQuery.of(context).size;
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final wheelData = _WheelData(
+          constraints: constraints,
+          itemCount: items.length,
+          textDirection: Directionality.of(context),
+        );
 
-              return Transform.rotate(
-                angle: (4 * _math.pi) / items.length,
-                child: LayoutBuilder(
-                  builder: (context, constraints) {
-                    final wheelData = _WheelData(
-                      constraints: constraints,
-                      itemCount: items.length,
-                      textDirection: Directionality.of(context),
-                    );
+        final transformedItems = [
+          for (var i = 0; i < items.length; i++)
+            TransformedFortuneItem(
+              item: items[i],
+              angle: _calculateSliceAngle(i, items.length),
+              offset: wheelData.offset,
+            ),
+        ];
 
-                    final transformedItems = [
-                      for (var i = 0; i < items.length; i++)
-                        TransformedFortuneItem(
-                          item: items[i],
-                          angle: _calculateSliceAngle(i, items.length),
-                          offset: wheelData.offset,
-                        ),
-                    ];
-
-                    return SizedBox.expand(
-                      child: _CircleSlices(
-                        items: transformedItems,
-                        wheelData: wheelData,
-                        styleStrategy: UniformStyleStrategy(),
-                      ),
-                    );
-                  },
-                ),
-              );
-            },
+        return SizedBox.expand(
+          child: _CircleSlices(
+            items: transformedItems,
+            wheelData: wheelData,
+            styleStrategy: UniformStyleStrategy(),
           ),
-          // child: FortuneWheel(
-          //   alignment: alignment.value,
-          //   selected: selected.stream,
-          //   onAnimationStart: () => isAnimating.value = true,
-          //   onAnimationEnd: () => isAnimating.value = false,
-          //   onFling: handleRoll,
-          //   indicators: [
-          //     FortuneIndicator(
-          //       alignment: alignment.value,
-          //       child: TriangleIndicator(),
-          //     ),
-          //   ],
-          //   items: [
-          //     for (var it in Constants.fortuneValues)
-          //       FortuneItem(child: Text(it), onTap: () => print(it))
-          //   ],
-          // ),
-        ),
-      ],
+        );
+      },
     );
   }
 }
